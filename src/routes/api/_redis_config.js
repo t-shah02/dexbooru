5e17999d7fc5df96ccab2949c51c7a1037200a24
tanny <connecttanish@gmail.com>
2022-06-25 04:02:27 -0700

    logging

--- src/routes/api/_redis_config.js
+++ src/routes/api/_redis_config.js
@@ -1,6 +1,7 @@
 import Redis from "ioredis";
 
 const REDIS_URL = process.env.NODE_ENV !== "production" ? import.meta.env.VITE_REDIS_URL : process.env.REDIS_URL;
+console.log(REDIS_URL);
 
 const redisClient = new Redis(REDIS_URL);
 
