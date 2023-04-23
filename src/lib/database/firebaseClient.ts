4e7037b227e93eb53069799df515cfda2dad07ae
root <root@LAPTOP-19FB0DP5>
2023-04-22 18:16:31 -0600

    added recaptcha key

--- src/lib/database/firebaseClient.ts
+++ src/lib/database/firebaseClient.ts
@@ -1,7 +1,8 @@
 import { initializeApp } from 'firebase/app';
 import { connectFirestoreEmulator, getFirestore } from 'firebase/firestore';
+import { initializeAppCheck, ReCaptchaV3Provider } from 'firebase/app-check';
 
-const firebaseConfig = {
+const FIREBASE_CONFIG = {
 	apiKey: 'AIzaSyC2lGfv19kaemnXfCNFHxlid0lOsHfMxuQ',
 	authDomain: 'dexbooru.firebaseapp.com',
 	databaseURL: 'https://dexbooru-default-rtdb.firebaseio.com',
@@ -12,7 +13,14 @@ const firebaseConfig = {
 	measurementId: 'G-LJ3FFSEVSF'
 };
 
-const firebaseApp = initializeApp(firebaseConfig);
+const RECAPTCHA_SITE_KEY = '6Le4F60lAAAAAN0agDhBJH2z8-d50p1MevIDubkE';
+
+const firebaseApp = initializeApp(FIREBASE_CONFIG);
+const firebaseAppCheck = initializeAppCheck(firebaseApp, {
+	provider: new ReCaptchaV3Provider(RECAPTCHA_SITE_KEY),
+	isTokenAutoRefreshEnabled: true
+});
+
 const fireDb = getFirestore(firebaseApp);
 
 export default fireDb;
